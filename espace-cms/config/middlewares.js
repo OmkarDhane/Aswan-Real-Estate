0b4178de2ea16ce8dca068fbd204de3009779ba7
OmkarDhane <omkardhane159@gmail.com>
2026-01-03 12:18:02 +0530

    added

---
+++ espace-cms/config/middlewares.js
@@ -0,0 +1,35 @@
+export default [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: "256mb",
+      jsonLimit: "256mb",
+      textLimit: "256mb",
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024,
+      },
+    },
+  },
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
--- espace-cms/config/middlewares.ts
+++ espace-cms/config/middlewares.ts
@@ -1,35 +1,11 @@
 export default [
+  'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
-  'strapi::logger',
   'strapi::query',
   'strapi::body',
-  {
-    name: 'strapi::body',
-    config: {
-      formLimit: "256mb",
-      jsonLimit: "256mb",
-      textLimit: "256mb",
-      formidable: {
-        maxFileSize: 250 * 1024 * 1024,
-      },
-    },
-  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
