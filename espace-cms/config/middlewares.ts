2cc754fe9bef4aaf7e0e74b1c9c6c11345add3fb
OmkarDhane <omkardhane159@gmail.com>
2026-01-05 10:42:44 +0530

    u

--- espace-cms/config/middlewares.ts
+++ espace-cms/config/middlewares.ts
@@ -1,11 +1,35 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+  name: 'strapi::security',
+  config: {
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        'connect-src': ["'self'", 'https:'],
+        // 'res.cloudinary.com' सोबतच 'https:' सुद्धा ॲड करा
+        'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'https:'],
+        'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'https:'],
+        upgradeInsecureRequests: null,
+      },
+    },
+  },
+},
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: "256mb",
+      jsonLimit: "256mb",
+      textLimit: "256mb",
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024,
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
